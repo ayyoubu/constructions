e8f2cab682b1089216646101c42b1288a0904698
Ayyoub <Ayyoub@AYYOUB-PC>
2019-12-16 15:50:52 +0000

    tested this at home

--- WindowsFormsApplication2/Form25.cs
+++ WindowsFormsApplication2/Form25.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication2
         {
             InitializeComponent();
         }
+
+        private void Form25_Load(object sender, EventArgs e)
+        {
+            //a test 
+        }
     }
 }
